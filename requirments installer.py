74b856a9427eec08d477452c4dce91bbb8d24920
Matheesha <pathiranamatheesha@gmail.com>
2022-03-15 14:41:49 +0530

    Initial commit

---
+++ requirments installer.py
@@ -0,0 +1,19 @@
+import os
+import time
+import requests
+
+try:
+    import requests
+except ImportError:
+    os.system('pip install requests')
+
+url = "https://google.com"
+try:
+    response = requests.get(url)
+    time.sleep(.4)
+    input('Press enter to install Requirements\n')
+    os.system('pip install -r requirements.txt')
+
+except requests.exceptions.ConnectionError:
+    input('You Need an Internet Connection!\npress enter to exit\n')
+    exit()
